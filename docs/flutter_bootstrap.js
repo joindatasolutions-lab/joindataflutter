941f25d2f401a2149387e8e71a315b5b847ebb25
juan de dios camacho <jdcamach@bancolombia.com.co>
2025-11-20 14:16:49 -0500

    Fix rutas con base href

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1375697435"
+    serviceWorkerVersion: "1598101126"
   }
 });
